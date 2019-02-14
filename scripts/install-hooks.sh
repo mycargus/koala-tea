0ab4aafb9f92d58a3a0d460aa71fda3f7c411e3d
Michael Hargiss <mdhargiss@gmail.com>
2019-02-13 19:35:21 -0500

    simplify install-hooks.sh, make pre-commit.sh executable

--- scripts/install-hooks.sh
+++ scripts/install-hooks.sh
@@ -2,32 +2,16 @@
 
 set -e
 
+path_to_pwd="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
 # shellcheck source=lib/binutil.bash
-source "$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/../lib/binutil.bash" &&
+source "$path_to_pwd/../lib/binutil.bash" &&
 ensure_running_from_root
 
 GIT_DIR="$(git rev-parse --git-dir)"
-OS_NAME="$(uname | tr '[:upper:]' '[:lower:]')"
-
-function read_link() {
-  case "${OS_NAME}" in
-    darwin)
-      exit_code="$(hash greadlink)"
-      if [ "$exit_code" == "0" ]; then
-        greadlink -f "$1"
-      else
-        echo "Install greadlink first. With homebrew you can: 'brew install coreutils'"
-      fi
-      ;;
-    *)
-      readlink -f "$1"
-      ;;
-  esac
-}
 
 echo "Installing hooks ..."
 
-path_to="$( dirname "$( dirname "$( read_link "${BASH_SOURCE[0]}" )" )" )"
-ln -s "$path_to/pre-commit.sh" "$GIT_DIR/hooks/pre-commit"
+ln -s "$path_to_pwd/pre-commit.sh" "$GIT_DIR/hooks/pre-commit"
 
 echo "Done!"
