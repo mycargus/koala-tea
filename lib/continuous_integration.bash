c917d07a83ab54e3b63eb801eb9b8694233b78ab
Michael Hargiss <mdhargiss@gmail.com>
2019-02-13 19:53:49 -0500

    lint bash

--- lib/continuous_integration.bash
+++ lib/continuous_integration.bash
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+lint_bash() {
+  bin/lint_bash -x \
+    bin/* \
+    lib/*.bash \
+    scripts/*
+}
+
 lint_markdown() {
   bin/lint_markdown --frail .
 }
--- scripts/pre-commit.sh
+++ scripts/pre-commit.sh
@@ -5,3 +5,4 @@ set -e
 project_root="$(dirname "$(git rev-parse --git-dir)")"
 
 bash "$project_root"/lib/continuous_integration.bash lint_markdown
+bash "$project_root"/lib/continuous_integration.bash lint_bash
